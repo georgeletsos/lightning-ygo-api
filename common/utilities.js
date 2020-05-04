5df10e9f59ca590ce091f49b658de57951a97db3
georgeletsos <georgeletsos391@gmail.com>
2020-05-04 12:23:45 +0300

    utilities: findLastIndex & spliceQuestionMarkCards

--- common/utilities.js
+++ common/utilities.js
@@ -2,6 +2,41 @@ const escapeRegExp = function(str) {
   return str.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
 };
 
+const findLastIndex = function(arr, callback) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (callback(arr[i])) {
+      return i;
+    }
+
+    continue;
+  }
+};
+
+// From an array of cards, splice those with "?" values based on sort field
+// And place them at the start or end of the array based on sort order
+const spliceQuestionMarkCards = function(cards, sortField, sortOrder) {
+  const hasQuestionMark = card => card[sortField] === "?";
+  const firstIndex = cards.findIndex(hasQuestionMark);
+  if (firstIndex === -1) {
+    return cards;
+  }
+
+  const lastIndex = findLastIndex(cards, hasQuestionMark);
+  const questionMarkCards = cards.splice(firstIndex, lastIndex + 1);
+
+  switch (sortOrder) {
+    case "asc":
+      cards.unshift(...questionMarkCards);
+      break;
+    case "desc":
+      cards.push(...questionMarkCards);
+      break;
+  }
+
+  return cards;
+};
+
 module.exports = {
-  escapeRegExp
+  escapeRegExp,
+  spliceQuestionMarkCards
 };
