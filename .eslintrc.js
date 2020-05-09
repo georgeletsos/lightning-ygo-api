002735093478f60fe7f990f4898a917a1a5754f7
georgeletsos <georgeletsos391@gmail.com>
2020-05-09 20:09:14 +0300

    ESLint config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    node: true,
-    es6: true
+    node: true
   },
   extends: ["plugin:prettier/recommended", "eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2017
+  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    ecmaVersion: 2017
   }
 };
