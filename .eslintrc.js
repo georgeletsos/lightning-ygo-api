c090cfa43d72c0419cc6cdab4b591a663fa03b63
georgeletsos <georgeletsos391@gmail.com>
2020-05-15 20:46:03 +0300

    Installed Jest and configured it

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,13 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)"],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
