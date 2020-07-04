56a6bb209a7cfd1692aa766793f2f82b9b5b53a4
georgeletsos <georgeletsos391@gmail.com>
2020-07-04 16:47:08 +0300

    Changed DLM API URLs

--- database.js
+++ database.js
@@ -51,7 +51,7 @@ const fetchMissingCards = async () => {
   // DLM Exclusive Cards, populate them with the CARD BACK image because they don't exist in ygopro api
   const dlmExclusiveCardsResponse = await axios
     .get(
-      "https://www.duellinksmeta.com/data-hashed/exclusiveCards-0bd3602ce7.json"
+      "https://www.duellinksmeta.com/data-hashed/exclusiveCards-7555762466.json"
     )
     .catch(error => {
       throw new Error(`DLM Exclusive Cards ${error}`);
@@ -70,7 +70,7 @@ const fetchMissingCards = async () => {
 
   // DLM All Cards
   const dlmAllCardsResponse = await axios
-    .get("https://www.duellinksmeta.com/data-hashed/cardObtain-892fdaaaea.json")
+    .get("https://www.duellinksmeta.com/data-hashed/cardObtain-c1711c65a9.json")
     .catch(error => {
       throw new Error(`DLM All Cards ${error}`);
     });
